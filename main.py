5efe9d34461ddf6a28e5138c8ca0ccbac1e79748
Praktikant ADP1 <praktikant.adp1@domel.com>
2024-07-01 13:27:59 +0200

    Fixed parsing

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import os
 import time
 from datetime import datetime
 
-def parse_duration(duration):
-    duration_pattern = re.compile(r'(?P<value>\d+)(?P<unit>[YMDhms])')
-    match = duration_pattern.fullmatch(duration.strip())
-    if not match:
-        raise ValueError(f"Invalid duration format: {duration}")
+def parse_duration(duration, unit):
+    #duration_pattern = re.compile(r'(?P<value>\d+)(?P<unit>[YMDhms])')
+    #match = duration_pattern.fullmatch(duration.strip())
+    #if not match:
+    #    raise ValueError(f"Invalid duration format: {duration}")
     
-    value = int(match.group('value'))
-    unit = match.group('unit')
+    #value = int(match.group('value'))
+    #unit = match.group('unit')
+    value = duration
     
     duration_dict = {
         'Y': value * 365 * 24 * 3600,  # Years to seconds
@@ -43,24 +44,26 @@ def delete_old_files_and_empty_folders(folder_path, time_threshold, initial_call
         elif os.path.isfile(item_path):
             creation_time = get_file_creation_time(item_path)
             if creation_time < time_threshold:
-                print(f"Deleting file: {item_path}")
+                #print(f"Deleting file: {item_path}")
                 os.remove(item_path)
 
     if not initial_call and not os.listdir(folder_path):
-        print(f"Deleting empty folder: {folder_path}")
+        #print(f"Deleting empty folder: {folder_path}")
         os.rmdir(folder_path)
 
 parser = argparse.ArgumentParser(description="Accept folder and duration.")
 parser.add_argument("folder", type=str, help="The folder path.")
-parser.add_argument("duration", type=str, help="The duration (e.g., '1Y', '3D', '300s').")
+parser.add_argument("duration", type=int, help="Age.")
+parser.add_argument("unit", type=str, help="Time unit of duration.")
 
 args = parser.parse_args()
 folder = args.folder
-duration = parse_duration(args.duration)
+duration = parse_duration(args.duration, args.unit)
 max_age = round(time.time() - duration)
 
-print(f"Folder: {folder}")
-print(f"Duration in seconds: {duration}")
-print(f"Max age in seconds: {max_age}")
+#print(f"Folder: {folder}")
+#print(f"Duration in seconds: {duration}")
+#print(f"Max age in seconds: {max_age}")
 
 delete_old_files_and_empty_folders(folder, max_age, True)
+print("Old files deleted")
